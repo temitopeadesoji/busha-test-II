c6f76a9bbc684156ffa5bb6cdab28b759e2027ae
davidpayne <temitopeadesoji23@gmail.com>
2019-09-23 22:24:34 +0100

    updated readme

--- src/Error.js
+++ src/Error.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="App-title">Welcome to error with Routing</h1>
+        <h1 className="App-title">404 Error Page</h1>
       </div>
     );
   }
